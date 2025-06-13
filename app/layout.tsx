472b62cbaea2da556db7f3193a36174a55c7d38f
Joaquin <jfarfantorres@gmail.com>
2025-06-13 15:29:34 -0400

    feat: add Google Analytics tag to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,16 @@ export default function RootLayout({
   return (
     <html lang="en" className="light">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1M0T2HXGZN"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1M0T2HXGZN');
+          `
+        }} />
         <script dangerouslySetInnerHTML={{
           __html: `
             // Set light mode as default
